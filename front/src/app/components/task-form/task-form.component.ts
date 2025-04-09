e16839e57718089b8510314eacb0a19cdd7c5282
=Alberto <=jlr0013@alu.medac.es>
2025-04-09 13:37:13 +0200

    Revert "Integrando docker"
    This reverts commit 8118d44e6960cce0c32fb76b4d5fb943579c5e90.

---
+++ front/src/app/components/task-form/task-form.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-task-form',
+  standalone: true,
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css'],
+  imports: [FormsModule],
+})
+export class TaskFormComponent {
+  title: string = '';
+  description: string = '';
+
+  @Output() taskCreated = new EventEmitter<Task>();
+
+  constructor(private taskService: TaskService) {}
+
+  addTask() {
+    if (!this.title) {
+      alert('El título es obligatorio');
+      return;
+    }
+
+    const newTask: Task = {
+      id: '', // Esto se generará en el backend
+      title: this.title,
+      description: this.description,
+      completed: false,
+    };
+
+    this.taskService.createTask(newTask).subscribe((task) => {
+      this.taskCreated.emit(task); // Emitir evento para que el componente padre sepa que se creó una tarea
+      this.title = '';
+      this.description = '';
+    }, error => {
+      console.error('Error al crear la tarea:', error);
+    });
+  }
+}
+
